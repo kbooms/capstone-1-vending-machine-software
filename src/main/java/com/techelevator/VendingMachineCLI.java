28714591bccc31952b10df5bf9da5efd038517f6
B Cameron Hunt <bcameronhunt@gmail.com>
2022-06-18 15:06:17 -0700

    Option 2 shows a new menu!!!

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,23 +1,32 @@
 package com.techelevator;
 
 import com.techelevator.view.Menu;
+import com.techelevator.view.PurchaseMenu;
 
 public class VendingMachineCLI extends LoadMachine {
 
-	//added Values
+	//static methods for [] defining
+	private static final String PURCHASE_MENU_FEED_MONEY = "Feed Money";
+	private static final String PURCHASE_MENU_SELECT_PRODUCT = "Select Product";
+	private static final String PURCHASE_MENU_FINISH = "Finish Transaction";
 
-	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
 
-	private static final String MAIN_MENU_OPTION_REPORT = "Sales Report";
-//ogcode
+	//Original menu code
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	//added Menu code
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String MAIN_MENU_OPTION_REPORT = "Sales Report";
+	//String[] builders
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT, MAIN_MENU_OPTION_REPORT };
+	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_FEED_MONEY,PURCHASE_MENU_SELECT_PRODUCT,PURCHASE_MENU_FINISH};
 
 	private Menu menu;
+	//new purchase menu item for run method
+	private PurchaseMenu purchaseMenu;
 
-	public VendingMachineCLI(Menu menu) {
-		this.menu = menu;
+	public VendingMachineCLI(Menu menu, PurchaseMenu purchaseMenu) {
+		this.menu = menu; this.purchaseMenu = purchaseMenu;
 	}
 
 	public void run() {
@@ -31,6 +40,7 @@ public class VendingMachineCLI extends LoadMachine {
 				showProducts(restock);
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+				String choice2 = (String) purchaseMenu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
 			}else if (choice.equals(MAIN_MENU_OPTION_EXIT)){
 				//end program
 			}else if (choice.equals(MAIN_MENU_OPTION_REPORT)){
@@ -38,10 +48,11 @@ public class VendingMachineCLI extends LoadMachine {
 			}
 		}
 	}
-
+//breakpoint in main. Need to instant purchaseMenu?
 	public static void main(String[] args) {
 		Menu menu = new Menu(System.in, System.out);
-		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		PurchaseMenu purchaseMenu = new PurchaseMenu(System.in, System.out);
+		VendingMachineCLI cli = new VendingMachineCLI(menu, purchaseMenu);
 		cli.run();
 	}
 }
---
+++ src/main/java/com/techelevator/view/PurchaseMenu.java
@@ -0,0 +1,93 @@
+package com.techelevator.view;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class PurchaseMenu extends Menu{
+    //scanner/print
+    private PrintWriter out;
+    private Scanner in;
+
+    //balance message
+    private String noFeedBalance = "Current Money Provided: " + "$0.00" ;
+
+    //balance
+    private double balance = 0;
+
+   //super constructor borrowed from menu
+    public PurchaseMenu(InputStream input, OutputStream output) {
+        super(input, output);
+        this.out = new PrintWriter(output);
+        this.in = new Scanner(input);
+    }
+
+    public Object getChoiceFromOptions(Object[] options) {
+        Object choice = null;
+        while (choice == null) {
+            displayMenuOptions(options);
+            choice = getChoiceFromUserInput(options);
+        }
+        return choice;
+    }
+
+    private Object getChoiceFromUserInput(Object[] options) {
+        Object choice = null;
+        String userInput = in.nextLine();
+        try {
+            int selectedOption = Integer.valueOf(userInput);
+            if (selectedOption > 0 && selectedOption <= options.length) {
+                choice = options[selectedOption - 1];
+            }
+        } catch (NumberFormatException e) {
+            // eat the exception, an error message will be displayed below since choice will be null
+        }
+        if (choice == null) {
+            out.println(System.lineSeparator() + "*** " + userInput + " is not a valid option ***" + System.lineSeparator());
+        }
+        return choice;
+    }
+
+    private void displayMenuOptions(Object[] options) {
+        out.println();
+        for (int i = 0; i < options.length; i++) {
+            int optionNum = i + 1;
+            out.println("(" + optionNum + ") " + options[i]);
+        }
+        out.print(System.lineSeparator() + "Please choose a number corresponding with the requested option: ");
+        out.flush();
+    }
+
+
+    //method for taking cash from customer
+    public double feedMoney(){
+        System.out.println("Insert whole dollar amounts. Does not Accept $50 or $100 bills. ");
+        Scanner userFeed = new Scanner(System.in);
+        String feed = userFeed.nextLine();
+
+        System.out.println("Balance is now: $" + Double.parseDouble(feed));
+        return Double.parseDouble(feed);
+    }
+
+    //get balance method
+    public double getBalance(){
+        double newBalance = feedMoney();
+        setBalance(newBalance);
+
+        if (balance > 0){
+            newBalance = balance + feedMoney();
+            setBalance(newBalance);
+        }
+
+        return balance;
+    }
+
+    //updating balance with new amount
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    //getter
+
+}
